f88fa160d2d8837122c75c0371b1f2c757bb6b86
Temel00 <temelaudio@gmail.com>
2026-03-11 15:54:37 -0700

    dashboard color cycle

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -45,7 +45,11 @@ const CardDescription = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("text-sm text-muted-foreground", className)} {...props} />
+  <div
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
 ));
 CardDescription.displayName = "CardDescription";
 
